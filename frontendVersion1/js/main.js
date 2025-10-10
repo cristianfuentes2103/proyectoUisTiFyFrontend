37c09516f8d24abefb9366b3f86d734ba3ec45a4
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2025-10-09 19:20:35 -0500

    Eliminando use strict

--- frontendVersion1/js/main.js
+++ frontendVersion1/js/main.js
@@ -1,5 +1,5 @@
 import { API_BASE_URL } from '../config.js';
-'use strict';
+
 document.addEventListener('DOMContentLoaded', () =>{
 
     // ============================================================================
